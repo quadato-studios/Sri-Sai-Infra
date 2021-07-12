a61b2eb9908a4d167bcd0d0a6605b79b6fad3168
Bhanu Prakash-PC <bhanuprakash.vangari@gmail.com>
2021-07-12 23:00:13 +0530

    switch styling changes

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -26,8 +26,8 @@ function Projects(props) {
           checkedIcon={false}
           boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
           activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
-          // height={20}
-          // width={48}
+          height={20}
+          width={48}
           className="react-switch"
           id="material-switch"
         />
