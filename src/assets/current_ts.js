42f44911f51523e5d819b367572fe50d190d9ae0
Bhanu Prakash Vangari <bhanu.vangari@popcornapps.com>
2021-08-25 20:31:33 +0530

    Added Photos nad made changes in index.css

--- src/assets/current_ts.js
+++ src/assets/current_ts.js
@@ -1,9 +1,9 @@
 import img1 from "../assets/1.jpg";
 import img2 from "../assets/2.jpg";
-import img3 from "../assets/3.jpg";
+import img3 from "../assets/Swapna residancy.jpeg";
 import img4 from "../assets/4.jpg";
-import img5 from "../assets/5.jpg";
-import img6 from "../assets/6.jpg";
+import img5 from "../assets/Pramila residancy.jpeg";
+import img6 from "../assets/Neharika homes.jpeg";
 import img7 from "../assets/6.jpg";
 const data = [
   {
