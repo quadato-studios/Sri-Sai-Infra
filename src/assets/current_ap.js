7dbdf2cdfc1304d502a124d1a42060f758713bef
Bhanu Prakash Vangari <bhanu.vangari@popcornapps.com>
2021-09-21 23:13:51 +0530

    Minor Fixes and Photos for AP

--- src/assets/current_ap.js
+++ src/assets/current_ap.js
@@ -1,7 +1,9 @@
-import img1 from "../assets/Error.svg";
+import img1 from "../assets/AP-1.jpeg";
+import img2 from "../assets/AP-2.jpeg";
+
 const data = [
-  
- { id: 1,
+  {
+    id: 1,
     title: "SHANTHI NILAYAM",
     address: "Bhanugudi Centre Kakinada, EG Dist, Andhra Pradesh",
     img: img1,
@@ -10,24 +12,25 @@ const data = [
     flatSize: "	1400 Sft",
     costOfProject: "Rs. 1.70 Crores",
     municipality: "KMDA",
-    status: "Ongoing",  
-    flats: "", 
+    status: "Ongoing",
+    flats: "",
     gmap: "",
-     description: "",
-    },
-    { id: 2,
-      title: "SREE ANJAYANAYAM",
-      address: "",
-      img: img1,
-      sqft: "",
-      noOfFlat: "",
-      flatSize: "",
-      costOfProject: "",
-      municipality: "KMDA",
-      status: "Ongoing",  
-      flats: "", 
-      gmap: "",
-       description: "",
-      },
+    description: "",
+  },
+  {
+    id: 2,
+    title: "SREE ANJAYANAYAM",
+    address: "",
+    img: img2,
+    sqft: "",
+    noOfFlat: "",
+    flatSize: "",
+    costOfProject: "",
+    municipality: "KMDA",
+    status: "Ongoing",
+    flats: "",
+    gmap: "",
+    description: "",
+  },
 ];
 export default data;
