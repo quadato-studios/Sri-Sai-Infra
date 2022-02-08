e90bd27815c7e722eadc43a00ae266d058cdfc03
bhanuprakash-vangari <bhanuprakash.vangari@gmail.com>
2022-02-08 22:27:38 +0530

    Minor Fix

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -12,8 +12,12 @@ function Team() {
       </div>
       <div data-aos="fade-right">
         <p>
-          We are having experienced staff around 2 senior engineers of 30years
-          industry experience, 10 site engineers and 5 administration staff.
+          We have carefully cultivated a small team of gifted and diverse
+          individuals,all of whom are industry superstars. By being extremely
+          selective about our hires, we are able to invest a substantial amount
+          of resources to attract and retain top performers. We are having
+          experienced staff around 2 senior engineers of 30years industry
+          experience, 10 site engineers and 5 administration staff.
         </p>
       </div>
     </TeamContainer>
