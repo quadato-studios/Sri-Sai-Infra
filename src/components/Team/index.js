27857a3733834c2691db0fef35c2ce096a42df19
bhanuprakash-vangari <bhanuprakash.vangari@gmail.com>
2022-02-08 22:05:42 +0530

    Added Our Team Desc

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -12,10 +12,8 @@ function Team() {
       </div>
       <div data-aos="fade-right">
         <p>
-          We have carefully cultivated a small team of gifted and diverse
-          individuals,all of whom are industry superstars. By being extremely
-          selective about our hires, we are able to invest a substantial amount
-          of resources to attract and retain top performers.
+          We are having experienced staff around 2 senior engineers of 30years
+          industry experience, 10 site engineers and 5 administration staff.
         </p>
       </div>
     </TeamContainer>
